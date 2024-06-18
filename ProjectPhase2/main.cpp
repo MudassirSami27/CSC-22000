8c3b2d39991ab2ef139a56d2032467aa20e6324a
A. Khan <khanabdur901@gmail.com>
2024-06-18 16:54:00 -0400

    Added the Insert to Tail Function

--- ProjectPhase2/main.cpp
+++ ProjectPhase2/main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 using namespace std;
 
-//Creating a simple double linked list and testing functions
+// Creating a simple double linked list and testing functions
 struct Node{
     public:
         int Data;
-        Node* Right;
-        Node* Left;
+        Node* Right;  // Next node is right node
+        Node* Left;    // Previous node is left node
 };
+
+// Function to add a new node with value `data` to the tail of the list
+void AddToTail(Node*& tail, int data) {
+    Node* newNode = new Node;
+    newNode->Data = data;  
+    newNode->Right = NULL; // newNode is the tail so there should be nothing after
+    newNode->Left = tail; // left pointer of the newNode now points to tail node(newNode)
+    
+    if (tail != NULL) {
+        tail->Right = newNode; // if current tail is not empty. Sets the right pointer of the current tail to the newNode
+    }
+    tail = newNode; // updates tail as the newNode
+}
+
+// Function to print elements from head to tail
 void printElements(Node* n){
-    while (n != NULL)
-    {
+    while (n != NULL) {
         cout << n->Data << endl;
         n = n->Right;
     }
 }
+
+// Function to print elements from tail to head
 void printReverse(Node* x){
     while (x != NULL){
         cout << x->Data << endl;
@@ -23,7 +39,7 @@ void printReverse(Node* x){
 }
 
 int main(){
-    //initializing nodes
+    // Initializing nodes
     Node* head = new Node;
     Node* second = new Node;
     Node* third = new Node;
@@ -45,7 +61,25 @@ int main(){
     tail->Left = third;
     tail->Right = NULL;
 
+    cout << "BEFOR ADDING TO TAIL: " << endl;
+    cout << "Print elements: " << endl;
     printElements(head);
+
+    cout << "Print Reverse: " << endl;
     printReverse(tail);
+
+    // Adding elements to the tail
+    
+    AddToTail(tail, 15);
+
+    cout << "AFTER ADDING TO TAIL: " << endl;
+
+    cout << "Print elements: " << endl;
+    // Printing elements
+    printElements(head);
+
+    cout << "Print Reverse: " << endl;
+    printReverse(tail);
+
     return 0;
 }
