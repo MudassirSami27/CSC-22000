eed697eeb86d4c3f1a34c7365a57ffffc8392187
T-Eduardo <f.torres.eduardo@gmail.com>
2024-07-03 21:54:22 -0400

    Added Menu and node count function.

--- ProjectPhase3/main.cpp
+++ ProjectPhase3/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 
 struct Node
@@ -21,6 +23,7 @@ class BinaryTree{
     void insert(Node*& node, int val) {
         if (node == NULL) {
             node = new Node(val); //if tree is empty add a new node that holds the value user selected
+            count++;
         } else if (val < node->data) { 
             insert(node->left, val);
         } else { //if value is greater than the previous node set it as right child
@@ -30,7 +33,7 @@ class BinaryTree{
     void inorder(Node* node){ //will print the elements of the tree in chronological order (only for binary trees)
         if(node != NULL){
             inorder(node->left);
-            cout << node->data << " " << endl;
+            cout << node->data << " ";
             inorder(node->right);
         }
     }
@@ -38,17 +41,18 @@ class BinaryTree{
         if(node != NULL){
             postorder(node->left);
             postorder(node->right);
-            cout << node->data << " " << endl;
+            cout << node->data << " ";
         }
     }
     void preorder (Node* node){
         if(node != NULL){
-            cout << node->data << " " << endl;
+            cout << node->data << " ";
             preorder(node->left);
             preorder(node->right);
         }
     }
     public:
+    int count = 0;
     BinaryTree(){
         root = NULL; //initializes an empty tree
     }
@@ -65,30 +69,66 @@ class BinaryTree{
     void preorder(){
         preorder(root);
     }
+    int nodeCount(){
+        return count;
+    }
 };
 
 int main(){
+    srand( time(NULL) );
     BinaryTree tree;
-    tree.insert(4);
-    tree.insert(11);
-    tree.insert(21);
-    tree.insert(32);
-    tree.insert(30);
-    tree.insert(18);
-    tree.insert(7);
-    tree.insert(3);
-    tree.insert(10);
-    tree.insert(6);
-    tree.insert(25);
-    tree.insert(12);
-    tree.insert(2);
-    tree.insert(29);
-    tree.insert(27);
+    int nodes = (rand() % 20) + 1;
+    for(int i  = 1; i <= nodes; i++){
+        tree.insert((rand() % 40) + 1);
+    }
 
-    cout << "Inorder Traversal" << endl;
-    tree.inorder();
-    cout << "Postorder Traversal" << endl;
-    tree.postorder();
-    cout << "Preorder Traversal" << endl;
-    tree.preorder();
+    int num;
+    bool status = true;
+    
+    while(status){
+        cout << "Tree List Menu" << endl;
+        cout << "Option 1: Insert new number." << endl;
+        cout << "Option 2: Print Tree." << endl;
+        cout << "Option 3: Get Node count of the Tree." << endl;
+        cout << "Option 4: Get Leaf count of Tree." << endl;
+        cout << "Option 5: Generate new Tree." << endl;
+        cout << "Option 6: Exit Program." << endl;        
+        cout << "Enter a number 1-6:" << endl;
+        cin >> num;
+
+        switch(num){
+            case 1:
+                cout << "Enter value for new element..." << endl;
+                int n;
+                cin >> n;
+                cout << "Adding new element..." << endl;
+                tree.insert(n);
+                break;
+            case 2:
+                cout << "Printing Tree in Preorder, Inorder, and Postorder..." << endl;
+                cout << "Preorder Traversal: ";
+                tree.preorder();
+                cout << endl;
+                cout << "Inorder Traversal: "; 
+                tree.inorder();
+                cout << endl;
+                cout << "Postorder Traversal: ";
+                tree.postorder();
+                cout << endl;
+                break;
+            case 3: 
+                cout << "Node count of Tree is: " << tree.nodeCount() << endl;
+                break;
+            case 4:
+                cout << "Needs to be worked." << endl;
+                break;
+            case 5:
+                cout << "Needs to be worked." << endl;
+                break;
+            case 6:
+                cout << "Exiting...";
+                status = false;
+                break;
+        }
+    }
 }
